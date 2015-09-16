82adb370159fc83623f8d2a5f3c5257734f15221
Hüseyin Mert <huseyinin@gmail.com>
2015-09-16 21:13:09 +0300

    unused thraed Controller remove fix #9

--- src/Okulbilisim/FeedbackBundle/Controller/FeedbackController.php
+++ src/Okulbilisim/FeedbackBundle/Controller/FeedbackController.php
@@ -15,11 +15,6 @@ class FeedbackController extends Controller
         return $this->render('OkulbilisimFeedbackBundle:Feedback:inbox.html.twig');
     }
 
-    public function threadAction($threadId)
-    {
-        return $this->render('OkulbilisimFeedbackBundle:Feedback:thread.html.twig', array('threadId' => $threadId));
-    }
-
     public function newAction(Request $request)
     {
         $email = $request->get('email');
